ae7d738072532b438c01a8eea5e70e910f4188e4
Hannah1943 <yhchen000@gmail.com>
2024-03-06 22:12:05 +1300

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_hashing import Hashing
 
 app = Flask(__name__)
 app.secret_key = 'Summer*secrect@2024'
 app.config['UPLOAD_FOLDER'] = 'app/static/img/'
+hashing = Hashing(app)
 
 from app import login
 from app import admin
