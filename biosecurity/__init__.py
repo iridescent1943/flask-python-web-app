b95048c44367f182584518289447cab880b87b90
Hannah1943 <yhchen000@gmail.com>
2024-03-09 23:54:26 +1300

    minor changes

--- biosecurity/__init__.py
+++ biosecurity/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask_hashing import Hashing
 
 app = Flask(__name__)
 app.secret_key = 'Summer*secrect@2024'
-app.config['UPLOAD_FOLDER'] = 'biosecurity/static/img/'
+app.config['UPLOAD_FOLDER'] = 'app/static/img/'
 
 from biosecurity import login
 from biosecurity import admin
