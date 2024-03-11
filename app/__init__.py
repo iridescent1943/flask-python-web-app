98a7cdaea5ca0d0076220df5a6288f677063bf3f
Hannah1943 <yhchen000@gmail.com>
2024-03-12 01:16:09 +1300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.secret_key = 'Summer*secrect@2024'
-app.config['UPLOAD_FOLDER'] = 'app/static/img/'
+app.config['UPLOAD_FOLDER'] = '/home/yinghua1157910/Biosecurity/app/static/img'
 
 from app import login
 from app import admin
